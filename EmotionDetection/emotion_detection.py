b78a7935cf329f8d5b628800c4a38c44826eb4e7
Richa Jha <richajha749@gmail.com>
2026-08-03 23:01:58 -0400

    Add error handling for blank text input

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -8,6 +8,17 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json=input_json, headers=headers)
 
+    # Handle blank entries / bad requests
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+
     # Convert the response text into a dictionary
     response_dict = json.loads(response.text)
 
